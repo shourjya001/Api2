60c3389ecf867194c4b8e6bd0049f5218c759794
shourjya001 <83701525+shourjya001@users.noreply.github.com>
2025-07-22 14:58:27 +0530

    Update Appquery.java

--- Appquery.java
+++ Appquery.java
@@ -1,15 +1,8 @@
 package com.socgen.riskweb.enums;
 
 public enum AppQueries {
-    // Existing queries
-    QRY_ACTIONCODE_TRUNCATE("TRUNCATE TABLE \"MAESNUMIPL\""),
-    QRY_PRIMARYROLE_TRUNCATE("TRUNCATE TABLE \"TMAESNUMIPL\""),
-    QRY_SAVE_PRIMARYROLE("INSERT INTO \"TMAESNUMIPL\" (\"entityId\",\"code\",\"subbookingId\") VALUES (?,?,?)"),
-    QRY_SAVE_ACTIONCODE("INSERT INTO \"WK_MAESTRO_PRIMROLE_DBE\" (\"bdrId\",\"businessEntity\") VALUES (?,?)"),
-    
-    // Updated queries for external registration
-    QRY_EXTERNAL_TRUNCATE("TRUNCATE TABLE \"TMAESNUMIPL_EXTERNAL\""),
-    QRY_SAVE_EXTERNAL("INSERT INTO \"TMAESNUMIPL_EXTERNAL\" (\"bdrId\",\"subBookingId\",\"subBookingName\",\"country\",\"createdDate\") VALUES (?,?,?,?,?)");
+    QRY_MAESTROPRC_TRUNCATE("TRUNCATE TABLE \"MAESTROPRC\""),
+    QRY_SAVE_MAESTROPRC("INSERT INTO \"MAESTROPRC\" (\"bdrId\",\"subBookingId\",\"subBookingName\",\"country\",\"createdDate\") VALUES (?,?,?,?,?)");
 
     private final String value;
 
